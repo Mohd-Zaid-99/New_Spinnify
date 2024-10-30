6f924a31a34b6fa1bbaed5821cd7ab3411853eef
Mohd.Zaid <mohd.zaid@WELLNESSHO.LOCAL>
2024-10-30 19:02:33 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes, Link, Navigate } from 'react-router-dom';
 import './App.css';
+// import Navbar from "./components/navbar"
+import Countdown from './screens/countdown';
+import StoreSpin from './screens/storeSpin';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="">
+        {/* <Navbar/> */}
+        <Routes>
+          <Route path="/" element={<Navigate to="/countdown" />} />
+          <Route path="/countdown" element={<Countdown />} />
+          <Route path="/storeSpin" element={<StoreSpin />} />
+        </Routes>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
